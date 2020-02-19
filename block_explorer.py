62dd9e054449f137d1f655216bd39dc3335919a3
Christian Cathro <cscathro@gmail.com>
2020-02-18 18:45:45 -0600

    started on basic html front page

--- block_explorer.py
+++ block_explorer.py
@@ -1,10 +1,9 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import time
 from time import localtime, strftime
+from flask import Flask, render_template
 
-from flask import Flask
 app = Flask(__name__)
-
 
 @app.route("/")
 def get_block():
@@ -12,17 +11,14 @@ def get_block():
     rpc_password="Lcctest1234"
     rpc_host="127.0.0.1"
     rpc_port="42069"
-    Active = True
-    last_block = {"hash":""}
     rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, rpc_host, rpc_port),timeout=15)
     best_block_hash = rpc_connection.getbestblockhash()
     new_block = rpc_connection.getblock(best_block_hash)
-
     return str(new_block)
 
-
-
+@app.route("/")
+def main():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
-    
